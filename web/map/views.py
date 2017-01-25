80a7c27f9a84e31edff677af7d5605eb98ed6e5c
Farhad <farhad.kzm@gmail.com>
2017-01-25 14:11:27 +1100

    refactored elasticsearch query to include more search parameters

--- web/map/views.py
+++ web/map/views.py
@@ -1,30 +1,49 @@
-import requests
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.template import loader
 from elasticsearch import Elasticsearch
 
 es = Elasticsearch(['search:9200'], max_retries=10)
-def search(request):
 
+
+# search queries to be implemented:
+#
+def search(request):
 
     geo_piont = {
         "lat": float(request.GET.get('lat', 0)),
         "lon": float(request.GET.get('lng', 0))
     }
     distance = request.GET.get('distance', '')
+    category = request.GET.get('category', '')
+    name = request.GET.get('name', '')
+    occupation = request.GET.get('occupation', '')
+    clinic = request.GET.get('clinic', '')
+    matching_blocks = []
+    if name != '':
+        matching_blocks.append({"match": {"name": name}})
+
+    if occupation != '':
+        matching_blocks.append( {"match": {"detail.occupation": occupation}})
+
+    if clinic != '':
+        matching_blocks.append({"match": {"location.clinic_name": clinic}})
+
+    print 'printing matching blocks'
+    print matching_blocks
     search_body = {
         "query": {
             "bool": {
-                "must": {
-                    "match_all": {}
-                },
-                "filter": {
-                    "geo_distance": {
-                        "distance": distance,
-                        "location.geo_set": geo_piont
-                    }
-                }
+                "must": matching_blocks,
+                "filter": [
+                    {
+                        "geo_distance": {
+                            "distance": distance,
+                            "location.geo_set": geo_piont
+                        }
+                    },
+                    {"term": {"type": category}}
+                ]
             }
         }
     }
